812bcaa667119558f56e17b2d6ec4be5f371dc55
Wafic Alayli <wafikalayli2014@gmail.com>
2023-12-09 12:32:44 +0200

    a lot of stuff

---
+++ Code/php/change_fname.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Confirmation Page of Settings Change</title>
+    </head>
+
+    <body>
+        <h1>Confirmation Page of Customer Info</h1>
+
+        <p>You have successfully changed your name. 
+
+        <p>Below is a summary of the information you provided.<br><br>  
+        <?php
+
+        echo 'First Name: ' . $_POST ["FirstName"] . '<br>';
+
+        ?>
+    </body>
+</html>
---
+++ Code/php/change_password.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Confirmation Page of Settings Change</title>
+    </head>
+
+    <body>
+        <h1>Confirmation Page of Customer Info</h1>
+
+        <p>You have successfully changed your password. 
+
+        <p>Below is a summary of the information you provided.<br><br>  
+        <?php
+
+        echo 'Password: ' . $_POST ["Password"] . '<br>';
+
+        ?>
+    </body>
+</html>

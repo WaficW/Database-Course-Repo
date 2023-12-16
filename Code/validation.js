f21357be4f5e3758c62115efbd558692bd3cd598
Wafic Alayli <wafikalayli2014@gmail.com>
2023-12-16 13:50:12 +0200

    eddited validation.js

--- Code/validation.js
+++ Code/validation.js
@@ -20,14 +20,10 @@ validation
             rule: "email"
         },
         {
-            validator: (value) => () => {
-                return fetch("validate-email.php?email=" + encodeURIComponent(value))
-                       .then(function(response) {
-                           return response.json();
-                       })
-                       .then(function(json) {
-                           return json.available;
-                       });
+            validator: async (value) => {
+                const response = await fetch("validate-email.php?email=" + encodeURIComponent(value));
+                const json = await response.json();
+                return json.available;
             },
             errorMessage: "email already taken"
         }

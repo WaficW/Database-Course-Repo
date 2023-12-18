1aee9fa06eff917200d3db5c56a257e0d62c80fa
OmarAlTaki1 <omar.altaki@lau.edu>
2023-12-18 21:28:42 +0200

    added athlete account creation logic

---
+++ Code/athlete-signup.php
@@ -0,0 +1,106 @@
+<?php
+
+session_start();
+
+if (empty($_POST["firstName"])) {
+    die("First ame is required");
+}
+
+if (empty($_POST["lastName"])) {
+    die("Last name is required");
+}
+
+if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die("Valid email is required");
+}
+
+if (strlen($_POST["password"]) < 8) {
+    die("Password must be at least 8 characters");
+}
+
+if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
+    die("Password must contain at least one letter");
+}
+
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
+    die("Password must contain at least one number");
+}
+
+if (empty($_POST["number"])) {
+    die("Phone number is required");
+}
+
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__ . "/demo.php";
+
+$sql = "INSERT INTO registration (firstName, lastName, status, email, password, number)
+        VALUES (?, ?, 'a', ?, ?, ?)";
+        
+$stmt = $mysqli->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("ssssi",
+                  $_POST["firstName"],
+                  $_POST["lastName"],
+                  $_POST["email"],
+                  $password_hash,
+                  $_POST["number"],);
+
+if (!$stmt->execute()) {
+    if ($mysqli->errno === 1062) {
+        die("email already taken");
+    } else {
+        die($mysqli->error . " " . $mysqli->errno);
+    }
+}
+
+$sql2 = sprintf("SELECT * FROM registration WHERE email='%s'", $mysqli->real_escape_string($_POST["email"]));
+
+$result = $mysqli->query($sql2);
+
+$userID = $result->fetch_assoc();
+
+if($_POST["sport"] === "football"){
+    $sql1 = "INSERT INTO athlete (id, height, weight, teamID, position, dob)
+    VALUES (?, ?, ?, 1, ?, ?)";
+} elseif($_POST["sport"] === "basketball"){
+    $sql1 = "INSERT INTO athlete (id, height, weight, teamID, position, dob)
+    VALUES (?, ?, ?, 2, ?, ?)";
+} elseif($_POST["sport"] === "tabletennis"){
+    $sql1 = "INSERT INTO athlete (id, height, weight, teamID, position, dob)
+    VALUES (?, ?, ?, 3, ?, ?)";
+}
+
+$stmt1 = $mysqli->stmt_init();
+
+if(!$stmt1->prepare($sql1)){
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt1->bind_param("iddss",
+                   $userID["id"],
+                   $_POST["height"],
+                   $_POST["weight"],
+                   $_POST["position"],
+                   $_POST["dob"]);
+
+if ($stmt1->execute()) {
+
+    $_SESSION["message"] = "Athlete " . $_POST["firstName"] . " was added successfully";
+
+    header("Location: /github%20repos/Database-Course-Repo/Code/create-athlete-account.html");
+    exit;
+    
+} else {
+    
+    if ($mysqli->errno === 1062) {
+        die("coach is coaching another team");
+    } else {
+        die($mysqli->error . " " . $mysqli->errno);
+    }
+}
--- Code/create-athlete-account.php
+++ Code/create-athlete-account.php
@@ -1,3 +1,26 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["user_id"])) {
+    
+    $mysqli = require __DIR__ . "/demo.php";
+    
+    $sql = "SELECT * FROM registration
+            WHERE id = {$_SESSION["user_id"]}";
+            
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+
+    $isRole = $user["status"] ==='c';
+} else {
+    header("Location: member-homepage.php");
+    exit;
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,10 +31,11 @@
     <title>Sports Facility Sign-in</title>
     <link rel="stylesheet" href="Styles/style_createathacc.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
 </head>
-
-<!--CHANGE BUTTON TO COACH HOMEPAGE-->
 
 <body>
     <a href="coach-homepage.html">
@@ -23,7 +47,19 @@
     <div class="box">
         <form action="athlete-signup.php" method="post">
             <h1>Create Athlete Account</h1>
-
+            <?php 
+    
+                if(isset($_SESSION['message']))
+                {
+                    ?>
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong></strong> <?= $_SESSION['message']; ?>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    <?php 
+                unset($_SESSION['message']);
+            }
+            ?>
             <div class="input">
                 <input type="text" placeholder="First Name" required title="First Name" id="firstName" name="firstName"/>
                 <i class='bx bx-user'></i>
